f454499cc17949903cf6d128e32b35f80c2b2b72
serabuyukgoz <sera.buyukgoz@postgrad.plymouth.ac.uk>
2017-05-22 20:29:34 +0100

    Final version
    Final version of Java Socket Server and Ros Publisher, unnecessary lines deleted such as prints and comments added

---
+++ Python/rosPublisher.py
@@ -0,0 +1,32 @@
+import sys
+import rospy
+from std_msgs.msg import Int8
+
+#Function for changing TCP server string data to subcribing interger data
+def decideValue(msg):
+    if  msg == "no":
+        return 0
+    elif msg == "front":
+	return 1
+    elif msg == "back":
+	return 2
+    elif msg == "frontleft":
+	return 11
+    elif msg == "frontright":
+	return 12
+    elif msg == "backleft":
+	return 21
+    elif msg == "backright":
+	return 22
+    else:
+	return 0
+		
+#that funtion is main ros publisher		
+def rosPub(msg):
+    value = decideValue(msg);
+    pub = rospy.Publisher('chatter', Int8, queue_size=10) #defining ros publisher in integer
+    rospy.init_node('talker', anonymous=True) #defining nodes of ros
+	
+    pub.publish(value) #publishing value
+
+
--- Python/server.py
+++ Python/server.py
@@ -1,20 +1,19 @@
 import socket
 from rosPublisher import rosPub
 
+# this function is listening java socket
+# when a message received send that data to ros publisher
 def javaServer():
 
-        soc = socket.socket()
+        soc = socket.socket() #defining java socket
 	host = ""
 	port = 2004
-	soc.bind((host, port))
-	soc.listen(5)
+	soc.bind((host, port)) #defining IP Adrress and port
+	soc.listen(5) #wait for client
 	while True:
 		conn, addr = soc.accept()
-		print ("Got Connection from", addr)
-		msg = conn.recv(1024)
-		print(msg)
-		rosPub(msg)
-	
+		msg = conn.recv(1024) #mesages received
+		rosPub(msg) #mesage sended to ros publisher
 	
 if __name__ == '__main__':
 	javaServer()
